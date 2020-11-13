ffed28cc2cc0f1a3652dbff6a2aac8273e5c4cd8
Andres <andressocorro1@gmail.com>
2020-11-12 18:50:37 -0600

    added dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,42 @@
-d3.json("./data/samples.json").then((data) =>{
-    console.log(data);
+function getPlotID(id) {
+    d3.json("./data/samples.json").then((data) =>{
+        console.log(data);
 
-    console.log(data.samples[0].otu_ids.slice(0,10))
-    console.log(data.samples[0].sample_values.slice(0,10))
+        var ids = data.samples[0].otu_ids;
+        console.log(ids)
 
-
-
-
-
-
+        // console.log(data.samples[0].otu_ids.slice(0,10))
+        // console.log(data.samples[0].sample_values.slice(0,10))
 
 
 
 
-});
 
+
+
+
+
+
+    });
+};
+
+// getPlotID(940);
+
+// Default rendering of page
+function init(){
+    var dropdown = d3.select("#selDataset");
+
+    d3.json("./data/samples.json").then((data) =>{
+        console.log(data)
+
+        data.names.forEach(function(name){
+            dropdown.append("option").text(name).property("value");
+        });
+
+        getPlotID(data.names[0]);
+
+    });
+ 
+}
+
+init();
